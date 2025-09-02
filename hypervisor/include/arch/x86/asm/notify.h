18c148f6bc52e3e2c4c6fbe26648d20452fc8d31
Shiqing Gao <shiqing.gao@intel.com>
2025-09-02 12:39:17 +0800

    hv: smpcall: abstract a common SMP call framework
    This patch:
     - abstracts the common logic from existing x86 implementation
     - moves x86-specific logic to arch/x86/notify.c
    
    A new common/notify.{c,h} is introduced to provide a common SMP call framework for
    multi-arch support in ACRN.
    arch-specific files such as arch/{x86,riscv}/notify.c is aim to provide the
    corresponding implementations respectively.
    
    The framework provides the following common APIs:
     - init_smp_call(): initialize the SMP call support during pCPU initialization
     - handle_smp_call(): execute the SMP call notification handler
     - smp_call_function(): trigger the SMP call request to target pCPUs
    
    Other SW modules should invoke these common APIs to perform arch-independent
    SMP operations.
    
    Two arch-specific hooks are abstracted:
     - arch_smp_call_kick_pcpu():
        - On x86, special handling is required when LAPIC is passthrough.
        - On RISC-V, a plain IPI is sufficient to kick the target pCPU.
     - arch_init_smp_call():
       - On x86, CPU initialization reserves dedicated vectors and
         registers callback handlers for purposes such as notifications
         or posted interrupts.
       - On RISC-V, no special handling is required at present; this
         can be extended in the future if needed.
    
    ----------
    Changelog:
     * Merged the following two patches into one:
         [RFC PATCH v2 4/7] hv: introduce common/smp.{c,h}
         [RFC PATCH v2 5/7] hv: smpcall: x86: adapt to common SMP call
    Tracked-On: #8786
    Signed-off-by: Shiqing Gao <shiqing.gao@intel.com>
    Acked-by: Wang, Yu1 <yu1.wang@intel.com>

--- hypervisor/include/arch/x86/asm/notify.h
+++ hypervisor/include/arch/x86/asm/notify.h
@@ -1,23 +1,16 @@
 /*
- * Copyright (C) 2020-2022 Intel Corporation.
+ * Copyright (C) 2020-2025 Intel Corporation.
  *
  * SPDX-License-Identifier: BSD-3-Clause
+ *
  */
 
-#ifndef NOTIFY_H
-#define NOTIFY_H
+#ifndef X86_NOTIFY_H
+#define X86_NOTIFY_H
 
-typedef void (*smp_call_func_t)(void *data);
-struct smp_call_info_data {
-	smp_call_func_t func;
-	void *data;
-};
+#include <types.h>
 
-struct acrn_vm;
-void smp_call_function(uint64_t mask, smp_call_func_t func, void *data);
+void arch_init_smp_call(void);
+void arch_smp_call_kick_pcpu(uint16_t pcpu_id);
 
-void setup_notification(void);
-void handle_smp_call(void);
-void setup_pi_notification(void);
-
-#endif
+#endif /* X86_NOTIFY_H */
