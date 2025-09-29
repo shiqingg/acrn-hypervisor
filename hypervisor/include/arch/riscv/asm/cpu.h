shiqingg/acrn-hypervisor

commit a7239d1268a490c447a380a35a49a8fe9c3b39e2

[FIXME] hv: risc-v add denpended implementation in cpu.h
spinlock invokes CPU_INT_ALL_DISABLE & CPU_INT_ALL_RESTOE, bitops
invokes BITS_PER_LONG, add them to pass the build.

current implementation is macro, same as X86. later official mult-arch
change should refactor to static inline on both x86 and risc-v.

Tracked-On: #8803
Signed-off-by: Haicheng Li <haicheng.li@linux.intel.com>
Signed-off-by: Haoyu Tang <haoyu.tang@intel.com>
Reviewed-by: Yifan Liu  <yifan1.liu@intel.com>
Acked-by: Wang, Yu1 <yu1.wang@intel.com>

hypervisor/include/arch/riscv/asm/cpu.h
@@ -17,6 +17,12 @@
 #define cpu_relax()	barrier() /* TODO: replace with yield instruction */
 #define NR_CPUS		MAX_PCPU_NUM
 
+#define LONG_BYTEORDER 3
+#define BYTES_PER_LONG (1 << LONG_BYTEORDER)
+#define BITS_PER_LONG (BYTES_PER_LONG << 3)
+/* Define the interrupt enable bit mask */
+#define SSTATUS_SIE 0x2
+
 static inline uint16_t get_pcpu_id(void)
 {
 	/**
@@ -27,12 +33,29 @@ static inline uint16_t get_pcpu_id(void)
 }
 
 /* Write CSR */
-#define cpu_csr_write(reg, csr_val)					\
-({									\
-	uint64_t val = (uint64_t)csr_val;				\
-	asm volatile (" csrw " STRINGIFY(reg) ", %0 \n\t"		\
-			:: "r"(val): "memory");	 			\
-})
+#define cpu_csr_write(reg, csr_val)                                                                                    \
+	({                                                                                                             \
+		uint64_t val = (uint64_t)csr_val;                                                                      \
+		asm volatile(" csrw " STRINGIFY(reg) ", %0 \n\t" ::"r"(val) : "memory");                               \
+	})
+
+/**
+ * FIXME: to follow multi-arch design, refactor all of them into static inline functions with corresponding
+ *        X86 implementation together.
+ */
+#define local_irq_disable() asm volatile("csrc sstatus, %0\n" ::"i"(SSTATUS_SIE) : "memory")
+#define local_irq_enable() asm volatile("csrs sstatus, %0\n" ::"i"(SSTATUS_SIE) : "memory")
+#define local_save_flags(x) ({ asm volatile("csrr %0, sstatus, 0\n" : "=r"(x)::"memory"); })
+#define local_irq_restore(x) ({ asm volatile("csrs sstatus, %0\n" ::"rK"(x & SSTATUS_SIE) : "memory"); })
+#define local_irq_save(x)                                                                                              \
+	({                                                                                                             \
+		uint32_t val = 0U;                                                                                     \
+		asm volatile("csrrc %0, sstatus, 0\n" : "=r"(val) : "i"(SSTATUS_SIE) : "memory");                      \
+		*(uint32_t *)(x) = val;                                                                                \
+	})
+
+#define CPU_INT_ALL_DISABLE(x) local_irq_save(x)
+#define CPU_INT_ALL_RESTORE(x) local_irq_restore(x)
 
 void wait_sync_change(volatile const uint64_t *sync, uint64_t wake_sync);
 

commit a957ff451cd7c5b7da242df3d558abda62584467

hv: riscv: fix local_irq_save to only clear SIE bit
local_irq_save() is intended to read and clear the SIE bit in
sstatus (disabling interrupts). The current implementation mistakenly
clears all bits in sstatus.

This patch corrects the inline assembly so that only the SIE bit is affected,
ensuring proper interrupt save/disable semantics on RISC-V.

Fixes: a7239d126 ("[FIXME] hv: risc-v add denpended implementation in cpu.h")

Tracked-On: #8813
Signed-off-by: Shiqing Gao <shiqing.gao@intel.com>
Reviewed-by: Yifan Liu  <yifan1.liu@intel.com>
Acked-by: Wang, Yu1 <yu1.wang@intel.com>

hypervisor/include/arch/riscv/asm/cpu.h
@@ -133,7 +133,7 @@ static inline void arch_asm_pause(void)
 #define local_irq_save(x)                                                                                              \
 	({                                                                                                             \
 		uint32_t val = 0U;                                                                                     \
-		asm volatile("csrrc %0, sstatus, 0\n" : "=r"(val) : "i"(SSTATUS_SIE) : "memory");                      \
+		asm volatile("csrrc %0, sstatus, %1\n" : "=r"(val) : "i"(SSTATUS_SIE) : "memory");                     \
 		*(uint32_t *)(x) = val;                                                                                \
 	})
 